2957f96e53331f175b7c191d627dc4923cddce86
Daniel Maher <maherd2008@gmail.com>
2026-01-16 20:38:27 -0500

    Update decryptor.java

--- decryptor.java
+++ decryptor.java
@@ -2,103 +2,75 @@ import java.util.Random;
 import java.io.*;
 
 public class decryptor {
-    private int[] encBody;
-    private int[] keyBody;
-    private int encSize;
-    private int keySize;
+    private int[] encBody; //define the encoded image body array
+    private int[] keyBody; //define the key image body arra
+    private int encSize; //define the encoded image size
+    private int keySize; //define the key image size
 
-    private byte[] byteMsg;
+    private byte[] byteMsg; //define the byte array that will be written to the output file 
 
-    public decryptor(String originalImageName, String messageFileName, String encryptedFileName) {
+    public decryptor(String originalImageName, String messageFileName, String encryptedFileName) { //create the constructor
         try {
-            FileInputStream enc = new FileInputStream(encryptedFileName);
-            FileInputStream key = new FileInputStream(originalImageName);
+            FileInputStream enc = new FileInputStream(encryptedFileName); //open the input stream for the encoded image
+            FileInputStream key = new FileInputStream(originalImageName); //open the input stream for the key image
 
-            byte[] temp = new byte[54];
-            enc.read(temp);
-            encSize = readSize(temp);
+            byte[] temp = new byte[54]; //create a temp byte array of size 54 to read the header
+            enc.read(temp); //read the first 54 bytes of the bitmap (the header) and add it to temp
+            encSize = (int)temp[2]+(int)temp[3]*256+(int)temp[4]*256*256+(int)temp[5]*256*256*256-54; //get the encoded image size
 
-            key.read(temp);
-            keySize = readSize(temp);
+            key.read(temp); //read the header from the key image
+            keySize = (int)temp[2]+(int)temp[3]*256+(int)temp[4]*256*256+(int)temp[5]*256*256*256-54; //get the key image size
 
-            byte[] encBytes = new byte[encSize];
-            enc.read(encBytes);
-            encBody = convertToInt(encBytes, encSize);
+            byte[] encBytes = new byte[encSize]; //now that we have the size, define a byte array with the size found from the header
+            enc.read(encBytes); //read the body
+            encBody = encryptor.convertToInt(encBytes, encSize); //using the encryptor's convert to int method to convert the body to an int array
 
-            byte[] keyBytes = new byte[keySize];
+            byte[] keyBytes = new byte[keySize]; //same idea as lines 24-26
             key.read(keyBytes);
-            keyBody = convertToInt(keyBytes, keySize);
+            keyBody = encryptor.convertToInt(keyBytes, keySize); //convert the body to an int array
 
-            enc.close();
+            enc.close(); //to avoid data leaking close both input file streams
             key.close();
 
-            // Reverse the randomization
-            derandomize(keyBody, keySize);
+            encryptor.randomize(keyBody, keySize); //this is the same randomize function as in the encryptor with the same seed
 
-            decrypt(messageFileName);
+            decrypt(messageFileName); //calls the decrypt function
 
-        } catch (IOException e) {
+        } catch (IOException e) { //catch the file error if it exists and send it to the stack trace
             e.printStackTrace();
         }
     }
 
-    private int readSize(byte[] header) {
-        return (header[2] & 0xFF)
-                + ((header[3] & 0xFF) << 8)
-                + ((header[4] & 0xFF) << 16)
-                + ((header[5] & 0xFF) << 24)
-                - 54;
-    }
+    public void decrypt(String outFile) {
+        try { //try/catch this so it doesn't explode if something goes wrong
+            ByteArrayOutputStream msgStream = new ByteArrayOutputStream(); 
 
-    public int[] convertToInt(byte[] in, int len) {
-        int[] out = new int[len];
-        for (int i = 0; i < len; i++)
-            out[i] = in[i] & 0xFF;
-        return out;
-    }
+            int bpos = 0;
+            while (bpos + 7 < encBody.length) {
+                int cbyte = 0;
+                int pow = 1;
 
-    // Reverse of encryptor.randomize()
-    public void derandomize(int[] in, int len) {
-        long seed = (in[34] * 7 - 4) + (in[9] * 952 + 12);
-        Random random = new Random(seed);
+                for (int j = 0; j < 8; j++) {
+                    int bit = encBody[bpos] & 1;
+                    cbyte += bit * pow;
+                    pow <<= 1;
+                    bpos++;
+                }
 
-        for (int i = 0; i < len; i++) {
-            int randnum = random.nextInt(40) - 20;
+                if (cbyte == 0xFF) {
+                    break;
+                }
 
-            int val = in[i] + randnum;
-            if (val < 0) val = 0;
-            if (val > 255) val = 255;
-
-            in[i] = val;
-        }
-    }
-
-    public void decrypt(String outputFile) throws IOException {
-        ByteArrayOutputStream msgStream = new ByteArrayOutputStream();
-
-        int bpos = 0;
-        while (bpos + 7 < encBody.length) {
-            int cbyte = 0;
-            int pow = 1;
-
-            for (int j = 0; j < 8; j++) {
-                int bit = encBody[bpos] & 1;
-                cbyte += bit * pow;
-                pow <<= 1;
-                bpos++;
+                msgStream.write(cbyte);
             }
 
-            if (cbyte == 0xFF) {
-                break;
-            }
+            byteMsg = msgStream.toByteArray();
 
-            msgStream.write(cbyte);
+            FileOutputStream out = new FileOutputStream(outFile);
+            out.write(byteMsg);
+            out.close();
+        } catch(IOException e) {
+            e.printStackTrace();
         }
-
-        byteMsg = msgStream.toByteArray();
-
-        FileOutputStream out = new FileOutputStream(outputFile);
-        out.write(byteMsg);
-        out.close();
     }
 }
